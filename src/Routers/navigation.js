93c620e86e9c535bda6910ad798e179e6dbeebbd
pradeep <pradeepims89@gmail.com>
2019-12-16 20:48:21 +0530

    Add routing  and navigations

---
+++ src/Routers/navigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Button ,ButtonToolbar,Navbar,NavDropdown ,Nav} from 'react-bootstrap'
+
+class Navigations   extends React.Component{
+    render()
+    {
+       return(
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Home</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">About</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Logout
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+        
+       )
+        
+    }
+
+}
+export default Navigations;
---
+++ src/Routers/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Button ,ButtonToolbar,Navbar,NavDropdown ,Nav} from 'react-bootstrap'
+
+class Routers   extends React.Component{
+    render()
+    {
+       return(
+      
+        <Router>
+             <Route path={match.path} Component={}></Route>
+        </Router>
+        
+       )
+        
+    }
+
+}
+export default Routers;
