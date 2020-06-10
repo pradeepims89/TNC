830d00eb8834a4f838e0ea27b76ca35fb0111381
Pradip Kumar <pradeepims89@gmail.com>
2020-06-10 16:13:33 +0530

    Add h1 tag for test

--- src/component/test.jsx
+++ src/component/test.jsx
@@ -4,7 +4,8 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h4> This is the home page ok</h4>
+        <h3> This is the home page ok</h3>
+        <h1> This is the home page ok</h1>
       </div>
     )
 
