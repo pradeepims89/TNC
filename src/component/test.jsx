bbff5c4bfcefb51377982d68ae2fdddf3c4b13a5
Pradip Kumar <pradeepims89@gmail.com>
2020-06-10 16:10:38 +0530

    By pradeep

---
+++ src/component/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4> This is the home page ok</h4>
+      </div>
+    )
+
+  }
+
+}
